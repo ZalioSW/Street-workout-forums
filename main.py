a08f77aa90a1900dfb29db312c156756d7ce917b
ZalioSW <ztoxiumisntoxic@gmail.com>
2025-03-27 03:12:34 +0000

    login komit part 3

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ users = db.table('uporabniki')
 posts = db.table('objave')
 
 app = Flask(__name__)
+app.secret_key = "zaliosw_najjaci123"
 
 @app.route("/")
 def main_page():
@@ -22,25 +23,39 @@ def login_page():
             User = Query()
             user = users.search(User.username == username)
             if user:
-                if user['password'] == password:
+                if user[0]['password'] == password:
                     session['username'] = username
                     return jsonify({'success': True})
                 else:
                     return jsonify({'success': False, 'error': 'Wrong password'})
             else:
-                return "User not found. Please sign up."
+                return jsonify({'success': False, 'error': 'User not found. Please sign up.'})
         except Exception as e:
             print(f"Napaka pri prijavi: {str(e)}")
             return jsonify({'success': False, 'error': 'Prišlo je do napake'})
     return render_template("login.html")
 
+@app.route("/signup", methods=['GET', 'POST'])
+def signup_page():
+    if request.method == 'POST':
+        try:
+            username = request.form['username']
+            password = request.form['password']
+            users.insert({'username': username, 'password': password})
+            session['username'] = username
+            return jsonify({'success': True})
+        except Exception as e:
+            print(f"Napaka pri prijavi: {str(e)}")
+            return jsonify({'success': False, 'error': 'Prišlo je do napake'})
+    return render_template("signup.html")
+
+@app.route("/logout")
+def logout_page():
+    session.pop('username', None)
+    return redirect(url_for('login_page'))
 
 @app.route("/posts")
 def posts_page():
     return render_template("posts.html")
-
-@app.route("/signup")
-def signup_page():
-    return render_template("signup.html")
 
 app.run(debug=True)
